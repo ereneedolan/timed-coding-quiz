e3e714d28d22f7b8033b3ab6e1260322619f34c3
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-10-04 20:39:41 -0400

    added pseudocode to JS file

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 // DEPENDENCIES (DOM ELEMENTS)
 
+
 // DATA / STATE
+// array of words from which we will choose one randomly
+// need a variable to hold our chosen random word
+// variable to hold the character the user typed
+// variable to hold the actual timer
+// variable to hold the countdown of remaining seconds
+// variable to hold initials
+// Variable of number of games played
+// Variable to save score in local storage
 
 // FUNCTIONS
 
 // USER INTERACTIONS
 // A user can click...
+//The user starts the coding quiz by clicking the start button
+    // when the user click the start button a timer will start
+    // the first question will appear
+//When the user answers a question
+    //if the answer is correct another question will appear    
+    // if the answer is incorrect 10 seconds will be subtracted from the clock
+// The user answers as many questions as possible until all questiosn are answered or the time runs out
+    // if the timer reaches 0 the game is over
+// The user has completed the game either by a win or loss
+    // The user can save their initials and their score
+
 
 // INITIALIZATION
